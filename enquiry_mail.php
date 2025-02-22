8a42a7c6331174f5727b712e115d8c0013799dae
DML <48423549+bsadar@users.noreply.github.com>
2025-02-23 00:02:49 +0530

    test

---
+++ enquiry_mail.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = "dmlogics.testing@gmail.com.com";  // Change to your email
+    $subject = "New message from $name";
+    
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message sending failed.";
+    }
+}
+?>
